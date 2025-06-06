396cba20313942deb8ed238ee597efbf9a3153cb
David Yuan <wu.xiguanghua2014@gmail.com>
2025-06-06 21:57:07 +0800

    update

--- run_train.sh
+++ run_train.sh
@@ -7,8 +7,8 @@
 
 
 
-CUDA_VISIBLE_DEVICES=0 python train_qwen.py --training_mode encoder --use_lora --freeze_backbone --data_file data/va_pair_train.json
+CUDA_VISIBLE_DEVICES=0 python train_qwen.py --training_mode encoder --use_lora --freeze_backbone --data_file data/va_pair_balanced_train.json
 # 使用真实数据训练（指定GPU 0）
-CUDA_VISIBLE_DEVICES=0 python train_qwen.py --training_mode instruction --use_lora --data_file data/va_pair_train.json
+CUDA_VISIBLE_DEVICES=0 python train_qwen.py --training_mode instruction --use_lora --data_file data/va_pair_balanced_train.json
 
-CUDA_VISIBLE_DEVICES=0 python train_qwen.py --training_mode encoder --num_samples 50 --batch_size 1
+CUDA_VISIBLE_DEVICES=0 python train_qwen.py --training_mode encoder --num_samples 2 --batch_size 1
